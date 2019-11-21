e0c9fd95c7e89571d5e674981ded259474ad7bf9
Hotwing78 <dlwingo@gmail.com>
2019-11-20 20:54:44 -0500

    Project Object validation

--- src/main/java/com/fullstack/ppmtool/repositories/ProjectRepository.java
+++ src/main/java/com/fullstack/ppmtool/repositories/ProjectRepository.java
@@ -1,4 +1,12 @@
 package com.fullstack.ppmtool.repositories;
 
-public interface ProjectRepository {
+import com.fullstack.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
 }
--- src/main/java/com/fullstack/ppmtool/service/ProjectService.java
+++ src/main/java/com/fullstack/ppmtool/service/ProjectService.java
@@ -1,4 +1,19 @@
 package com.fullstack.ppmtool.service;
 
+import com.fullstack.ppmtool.domain.Project;
+import com.fullstack.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+
+    public Project saveOrUpdateProject(Project project){
+        //Logic
+        return projectRepository.save(project);
+    }
 }
